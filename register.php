9535c08ba5518eaf9bf2a149191dcb2433cf6a4c
younisbirahmani <114679911+younisbirahmani@users.noreply.github.com>
2022-10-01 00:46:25 +0500

    Add files via upload

--- register.php
+++ register.php
@@ -1,30 +1,35 @@
-<?php
-    include("connection.php");
+<html>
+    <head>
+        <title>Online voting system - Registratrion</title>
+        <link rel="stylesheet" href="../css/stylesheet.css">
+    </head>
+    <body>
+        <center>
+            <div id="headerSection">
+            <h1>Online Voting System</h1>  
+            </div>
+            <hr>
 
-    $name = $_POST['name'];
-    $mobile = $_POST['mob'];
-    $pass = $_POST['pass'];
-    $cpass = $_POST['cpass'];
-    $add = $_POST['add'];
-    $image = $_FILES['image']['name'];
-    $tmp_name = $_FILES['image']['tmp_name'];
-    $role = $_POST['role'];
-
-    if($cpass!=$pass){
-        echo '<script>
-                alert("Passwords do not match!");
-                window.location = "../routes/register.php";
-            </script>';
-    }
-    else{
-        move_uploaded_file($tmp_name,"../uploads/$image");
-        $insert = mysqli_query($connect, "insert into user (name, mobile, password, address, photo, status, votes, role) values('$name', '$mobile', '$pass', '$add', '$image', 0, 0, '$role') ");
-        if($insert){
-            echo '<script>
-                    alert("Registration successfull!");
-                    window.location = "../";
-                </script>';
-        }
-    }
-    
-?>
+            <h2>Registration</h2>
+                <form action="../api/register.php" method="POST" enctype="multipart/form-data">
+                    <input type="text" name="name" placeholder="Name" required>&nbsp
+                    <input type="number" name="mob" placeholder="Mobile" required><br><br>
+                    <input type="password" name="pass" placeholder="Password" required>&nbsp
+                    <input type="password" name="cpass" placeholder="Confirm Password" required><br><br>
+                    <input style="width: 31%" type="text" name="add" placeholder="Address" required><br><br>
+                    <div id="upload" style="width: 30%">
+                        Upload image: <input type="file" id="profile" name="image" required>
+                    </div><br>
+                    <div id="upload" style="width: 30%">
+                        Select your role:
+                        <select name="role">
+                            <option value="1">Voter</option>
+                            <option value="2">Group</option>
+                        </select><br>                   
+                    </div><br>
+                    <button id="loginbtn" type="submit" name="registerbtn">Register</button><br><br>
+                    Already user? <a href="../">Login here</a>
+                </form>
+            </center>
+    </body>
+</html>
